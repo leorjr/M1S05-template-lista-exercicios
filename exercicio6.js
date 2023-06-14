9a2871f3a64d777a4d26ef20f18f4014c739dcfc
leorjr <l.grodriguesjr@gmail.com>
2023-06-14 17:22:43 -0300

    feat: [M1S05] Ex 6 - Escreva um programa que percorra um array de objetos de produtos

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,34 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+
+const percorreArray = () =>{
+    const array = [
+        {
+            nome: "banana",
+            preco: 1.0
+        },
+        {
+            nome: "manga",
+            preco: 5.0
+        },
+        {
+            nome: "jaca",
+            preco: 10.0
+        },
+    ]
+
+    for(let produto of array){
+        console.log(`nome do produto: ${produto.nome} - valor do produto: R$ ${produto.preco}`)
+    }
+
+    const totalDeProdutos = array.length
+    console.log(`o total de produtos é: ${totalDeProdutos}`)
+
+    const somatorioDosValoresDosProdutos = array.reduce(
+        (accumulator, currentValue)=> accumulator + currentValue.preco, 0)
+    console.log(somatorioDosValoresDosProdutos)
+}
+
+percorreArray()
