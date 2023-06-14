a5b9a7be4c835b3ed03d55b731a0a178ef41d691
leorjr <l.grodriguesjr@gmail.com>
2023-06-14 17:15:40 -0300

    feat: [M1S05] Ex 5 - Escreva um programa que verifique um array de palavras

--- exercicio5.js
+++ exercicio5.js
@@ -4,3 +4,15 @@
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
  */
+
+const percorreArray = () =>{
+    const array = ["banana", "maçã", "maracujá", "kiwi", "morango"]
+
+    for(let item of array){
+        if(item.length > 5){
+            console.log(item)
+        }
+    }
+}
+
+percorreArray()
