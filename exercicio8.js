7aeb8c159eb7ffe3bb570ec8e35831f7dfd3dcdf
leorjr <l.grodriguesjr@gmail.com>
2023-06-15 10:53:25 -0300

    finalizados exercicios 8 e 9

--- exercicio8.js
+++ exercicio8.js
@@ -5,3 +5,22 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+
+class Calculadora{
+
+    somar(numero_1, numero_2){
+        return numero_1 + numero_2
+    }
+
+    subtrair(numero_1, numero_2){
+        return numero_1 - numero_2
+    }
+
+    multiplicar(numero_1, numero_2){
+        return numero_1 * numero_2
+    }
+
+    dividir(numero_1, numero_2){
+        return numero_1 / numero_2
+    }
+}
--- exercicio9.md
+++ exercicio9.md
@@ -1,3 +1,7 @@
 # [M1S05] Ex 9 - Conceito do GitFlow
 
 > Explique com suas palavras, o conceito do GitFlow e como ele é utilizado no controle de versão de um projeto.
+
+Gitflow é uma forma de controle e gerenciamento de versões, de forma padronizada, utilizada por muitas empresas onde, temos (geralmente), pelo menos 3 branchs: main - homolog - dev;
+
+A partir disso, todo o código é mandado da dev para a homolog e, caso esteja tudo certinho, quando for feito uma release do produto, o mesmo é mergeado na main;
